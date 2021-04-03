3927ba8b38601baf9ba3b77f2fd734de84cd956b
杨帅斌 <yangsb@iktapp.com>
2021-04-03 19:56:09 +0800

    我的部分，暂时功能，后续会继续往上加

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -2,6 +2,12 @@
   <div>
     <bottom-bar :type="'my'"></bottom-bar>
     <span>my</span>
+    <!-- 我的页面所需要的功能
+      1、喜欢功能：用来收藏菜谱
+      2、笔记功能：
+        查看对于菜谱做的笔记，会显示在（做笔记功能坐在菜谱的富文本当中，进行编辑）
+      3、创作并发布菜谱功能：用来发布自己制作的菜谱
+     -->
     <div>
       <van-cell-group>
         <van-cell title="单元格" value="内容"></van-cell>
