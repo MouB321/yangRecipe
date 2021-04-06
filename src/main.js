d0d760094de8e13d15ba527f4cab2f784d0cd4df
杨帅斌 <yangsb@iktapp.com>
2021-04-06 19:45:05 +0800

    引入axios和api封装，并完成测试接口连接

--- src/main.js
+++ src/main.js
@@ -15,6 +15,12 @@ import "../static/css/common.css"
 
 Vue.config.productionTip = false
 
+// 引入axios和api
+import axios from "../src/config/axios";
+Vue.prototype.$http = axios;
+import api from "../src/server/api";
+Vue.prototype.$api = api;
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
